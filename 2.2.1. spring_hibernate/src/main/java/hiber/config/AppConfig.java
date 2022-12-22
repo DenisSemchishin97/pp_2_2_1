95fee83d3b1e3095854f18acdc5f4f9917fcf2d7
DenisSemchishin97 <120304966+DenisSemchishin97@users.noreply.github.com>
2022-12-22 16:26:21 +0300

    Update AppConfig.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
@@ -23,8 +23,11 @@ import java.util.Properties;
 @ComponentScan(value = "hiber")
 public class AppConfig {
 
-   @Autowired
+
    private Environment env;
+   AppConfig(Environment env){
+      this.env=env;
+   }
 
    @Bean
    public DataSource getDataSource() {
